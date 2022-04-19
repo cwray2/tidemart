542d5ea831b701d598144beee42a6e13d5b3d02a
cwray2 <cwray2@crimson.ua.edu>
2022-04-19 18:53:30 -0500

    merge attempt

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -2,21 +2,21 @@ namespace api.Models
 {
     public class User
     {
-        public int UserID { get; set; }
+        public int UserID { get; set; } //this
         public string? Password { get; set; }
         public int SSN { get; set; }
-        public string? FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public DateTime StartDate { get; set; }
-        public string? Salary { get; set; }
+        public string? FirstName { get; set; } //this
+        public string? MiddleName { get; set; } //this
+        public string? LastName { get; set; } //this
+        public string? Email { get; set; } //this
+        public DateTime StartDate { get; set; } //this
+        public string? Salary { get; set; } //this
         public DateTime ShiftStart { get; set; }
         public DateTime Birthday { get; set; }
-        public string? Phone { get; set; }
-        public string? URL { get; set; }
-        public string? Admin { get; set; }
-        public string? Username { get; set; }
-        public int DepartmentID { get; set; }
+        public string? Phone { get; set; } //this
+        public string? URL { get; set; } //this
+        public string? Admin { get; set; } //this
+        public string? Username { get; set; } //this?
+        public int DepartmentID { get; set; } //this as department name
     }
 }
